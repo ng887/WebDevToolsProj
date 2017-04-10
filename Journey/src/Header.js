19eb5986ec6343ce9f32f96089cf55e547c48fc3
ng887 <nehagupta887@gmail.com>
2017-04-09 23:59:57 -0700

    add Header Component

---
+++ Journey/src/Header.js
@@ -0,0 +1,32 @@
+/**
+ * Created by neha on 4/9/2017.
+ */
+
+import React from 'react';
+import './App.css';
+
+const header = () => {
+
+
+    return (
+       <nav id="nav" className='j-card' >
+        <div className='desktopLayout' style={{justifyContent: 'space-between'}}>
+            <div><span className="text-xxxlarge j-button no-right-padding">J</span>            
+                 <span className='top-margin-small'>OURNEY</span>
+            </div>
+            <div id='down'>
+                <div>
+                    <a className='j-button top-margin' style={{marginTop:10}} >
+                        <i className="fa fa-bars" ></i>
+                    </a>
+                </div>            
+            </div>
+        </div>
+
+
+        
+        </nav>
+    )
+}
+
+export default header;
