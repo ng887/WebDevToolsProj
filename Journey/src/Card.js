a0112ea338b6d173da66475f7170cef0f3519681
ng887 <nehagupta887@gmail.com>
2017-04-12 20:37:29 -0700

    update card

--- Journey/src/Card.js
+++ Journey/src/Card.js
@@ -1,40 +1,30 @@
+/**
+ * Created by neha on 4/12/2017.
+ */
+
 import React from 'react';
 import noImg from './images/No_image_available.jpg';
 
-
 const Card = ({
-	noOfDays,
-    pointsOfInterest
+	pointsOfInterest,
+	i
 }) => {
-    console.log(pointsOfInterest);
-    console.log('hello');
-    console.log(pointsOfInterest[1]);
-
-	const tempDiv=[];
-
-    for(let i = 0; i < pointsOfInterest.length; i++) {
-     let photoUrl;
+	console.log(pointsOfInterest);
+	let photoUrl;
      // console.log(photoUrl);
-     if(pointsOfInterest[i].photos){
-      photoUrl = pointsOfInterest[i].photos[0].getUrl({maxWidth: 640});
+     if(pointsOfInterest.photos){
+      photoUrl = pointsOfInterest.photos[0].getUrl({maxWidth: 640});
      }
      else{
       photoUrl = noImg;
      }
-      tempDiv.push( 
-       <div  className='card' key={i}>
-           <span> <b>Name:</b> {pointsOfInterest[i].name}</span><br/>
-           <img src={photoUrl} alt='No Image'/>
-           <span> <b>Rating:</b> {pointsOfInterest[i].rating}</span><br/>
-        </div>
-      );
-    }   
-
+     
     return (
-        <div id="test">
-      {tempDiv}
-      </div>
+        <div className='card text-center' key={i}>
+           <div> <b> {pointsOfInterest.name}</b></div><br/>
+           <div><img src={photoUrl} alt='No Image' className="wideImg"/></div>
+           <div> <b>Rating:</b> {pointsOfInterest.rating}</div><br/>
+        </div>
     )
 }
-
 export default Card;
