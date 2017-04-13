ca7d0cff4bf2a1ae0589a2cb8de4cc68d81be37a
ng887 <nehagupta887@gmail.com>
2017-04-12 20:38:06 -0700

    update card Container

--- Journey/src/CardContainer.js
+++ Journey/src/CardContainer.js
@@ -1,32 +1,41 @@
 import React from 'react';
+import Card from './Card';
 
 const CardContainer = ({
-	noOfDays,
+    noOfDays,
     pointsOfInterest
 }) => {
 
-	const tempDiv=[];
-  //  if(pointsOfInterest) {
-     console.log(pointsOfInterest.length);
-    console.log(noOfDays);
-    //}
+    console.log(pointsOfInterest);
+    const tempDiv = [];
+    /*if(pointsOfInterest) {
+     //  console.log(pointsOfInterest.length);
+     // console.log(noOfDays);
+      //}*/
     let j = 0;
-    for(let i = 0; i < noOfDays; i++) {
-          tempDiv.push(
-              <div  className='card' key={i}>
-                <span>{pointsOfInterest[j].name}</span>
-                  <span>{pointsOfInterest[j+1].name}</span>
-                  <span>{pointsOfInterest[j+2].name}</span>
+    for (let i = 0; i < noOfDays; i++) {
+        if (!pointsOfInterest[j]) {
+            tempDiv.push(
+                <div className='card' key={i+1}>
+            <p> Sorry there are no other prominent places suggestions for the day. </p>
+            </div>
+            )
+        } else {
+            tempDiv.push(
+                <div className='card' key={i}>
+                  <Card pointsOfInterest={pointsOfInterest[j]} i={j}/>
+                  <Card pointsOfInterest={pointsOfInterest[j+1]} i={j+1}/>
+                  <Card pointsOfInterest={pointsOfInterest[j+2]} i={j+2}/>          
               </div>
-          );
+            );
+        }
         j += 3;
-    }   
+    }
 
     return (
-    	<div>
-      {tempDiv}
+      <div>
+          {tempDiv}
       </div>
     )
 }
-
 export default CardContainer;
