44a8d5b50accf89c7e34564f2816fcb66a8cab77
khutaija <shariff.k@husky.neu.edu>
2017-04-17 16:42:38 -0700

    adding keys to weather component

--- Journey/src/WeekWeatherForecast.js
+++ Journey/src/WeekWeatherForecast.js
@@ -18,9 +18,10 @@ const WeekWeatherForecast = ({
                 const date = (new Date(day.dt * 1000));
                 //console.log(date);
                 const dateTxt = date.toString().substring(4, 10);
+                let i = 0;
                 if (date.getHours() === 5) {
                     WeekWeatherForecast.push(
-                    <div className='text-center colLayout border marging padding brand-color3'>                 
+                    <div key={i++} className='text-center colLayout border marging padding brand-color3'>
 		 			<span> {dateTxt} 5AM</span>
 	                <img src={"http://openweathermap.org/img/w/"+day.weather[0].icon+".png"}  alt="weather_icon" />  <br/>
 	                <b> {day.weather[0].description} </b> <br/>
